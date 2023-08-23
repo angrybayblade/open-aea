5cdb8c565e1f8ba64e93013ec54a5d8179eb5b56
angrybayblade <vptl185@gmail.com>
2023-08-23 10:38:08 +0530

    refactor: dockerfiles develop-image/Dockerfile_local # deleted:  develop-image/docker-env.sh # deleted:  develop-image/k8s/deployment.yaml # deleted:  develop-image/skaffold.yaml # renamed:  deploy-image/Dockerfile -> dockerfiles/deploy/Dockerfile # renamed:  deploy-image/README.md -> dockerfiles/deploy/README.md develop-image/openssl.cnf -> dockerfiles/develop/openssl.cnf # new file:  dockerfiles/docs/Dockerfile # renamed:  user-image/Dockerfile -> dockerfiles/user/Dockerfile # renamed:  user-image/README.md -> dockerfiles/user/README.md # renamed:  user-image/openssl.cnf -> dockerfiles/user/openssl.cnf # deleted:  docs-image/Dockerfile # deleted:  skaffold.yaml # deleted:  user-image/docker-env.sh #

--- develop-image/docker-env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Swap the following lines if you want to work with 'latest'
-DOCKER_IMAGE_TAG=valory/open-aea-develop:1.38.0
-# DOCKER_IMAGE_TAG=valory/open-aea-develop:latest
-
-DOCKER_BUILD_CONTEXT_DIR=..
-DOCKERFILE=./Dockerfile
--- user-image/docker-env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Swap the following lines if you want to work with 'latest'
-DOCKER_IMAGE_TAG=valory/open-aea-user:1.38.0
-# DOCKER_IMAGE_TAG=valory/open-aea-user:latest
-
-DOCKER_BUILD_CONTEXT_DIR=..
-DOCKERFILE=./Dockerfile
